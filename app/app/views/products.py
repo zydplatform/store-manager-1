170c69a264569c34787d10cdef9f26cd5ffbf668
Walujo Emmanuel <walujoemmanuel@gmail.com>
2018-10-18 11:16:05 +0300

    Admin or Store Attendant can search and get a specific product [Finishes #161230083] [Finishes #161230015] [Finishes #161279418] [Finishes #161279446]

--- app/app/views/products.py
+++ app/app/views/products.py
@@ -1,5 +1,5 @@
 
-from flask import jsonify
+from flask import jsonify, abort
 from app import app
 from app.models import products
 
@@ -8,3 +8,14 @@ from app.models import products
 @app.route('/api/v1/attendant/products', methods=['GET'])
 def view_all_products():
     return jsonify({"Products": products}), 200
+
+
+@app.route('/api/v1/admin/products/<int:product_id>', methods=['GET'])
+@app.route('/api/v1/admin/attendant/<int:product_id>', methods=['GET'])
+def view_aproduct_details(product_id):
+    product = [product for product in products if product["product_id"] == product_id]
+
+    if len(product) == 0:
+        abort(404)
+
+    return jsonify({"Product": product[0]}), 200
