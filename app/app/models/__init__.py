b3261ab7912309a5142eddda51ac9588af8bfd16
Walujo Emmanuel <walujoemmanuel@gmail.com>
2018-10-18 11:10:26 +0300

    Added one more product

--- app/app/models/__init__.py
+++ app/app/models/__init__.py
@@ -4,11 +4,21 @@ from datetime import datetime
 
 time = str(datetime.now())
 
-products = [{
-            "product_id": 1,
-            "product_name": "Mangoes",
-            "product_category": "Fruits",
-            "product_price": 500,
-            "product_quantity": 34,
-            "product_minimum_stock_allowed": 20
-        }]
+products = [
+                {
+                    "product_id": 1,
+                    "product_name": "Mangoes",
+                    "product_category": "Fruits",
+                    "product_price": 500,
+                    "product_quantity": 34,
+                    "product_minimum_stock_allowed": 20
+                },
+                {
+                    "product_id": 2,
+                    "product_name": "Cabbage",
+                    "product_category": "Vegatables",
+                    "product_price": 1500,
+                    "product_quantity": 23,
+                    "product_minimum_stock_allowed": 20
+                }
+            ]
