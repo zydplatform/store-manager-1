b3939caea88bca6ff7268323d3703a80b43f65ab
Walujo Emmanuel <walujoemmanuel@gmail.com>
2018-10-18 23:10:27 +0300

    Adding tests for home route and add product route [Delivers #161279223]

--- app/tests/test_products.py
+++ app/tests/test_products.py
@@ -1,5 +1,28 @@
 
 import unittest
+from flask import json
+from app import app
+
+product = {
+            "product_id": 4,
+            "product_name": "Apple",
+            "product_category": "Fruits",
+            "product_price": 1000,
+            "product_quantity": 50,
+            "product_minimum_stock_allowed": 20
+        }
 
 class TestProducts(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.test_client = app.test_client()
+
+    def test_home(self):
+        response = self.test_client.get('/api/v1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Welcome To The Store Manager System API Version 1", response.data)
+
+    def test_add_product(self):
+        response = self.test_client.post('api/v1/admin/products', content_type="application/json",  data=json.dumps(product))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"The product has been added", response.data)
