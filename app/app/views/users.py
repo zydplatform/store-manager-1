bc6d2be8487b644ea38843b0322f7501cbab8b28
Walujo Emmanuel <walujoemmanuel@gmail.com>
2018-10-19 15:55:54 +0300

    admin gets a specific attendant details [Finishes #161317740] [Finishes #161318047]

--- app/app/views/users.py
+++ app/app/views/users.py
@@ -8,3 +8,11 @@ from app.models import attendants
 def view_all_store_attendants():
     return jsonify({"attendants": attendants}), 200
 
+@app.route('/api/v1/admin/attendants/<int:attendant_id>/', methods=['GET'])
+def view_astore_attendant_details(attendant_id):
+    attendant = [attendant for attendant in attendants if attendant["attendant_id"] == attendant_id]
+
+    if len(attendant) == 0:
+        abort(404)
+
+    return jsonify({"attendant": attendant[0]}), 200
