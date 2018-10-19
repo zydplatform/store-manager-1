7f5715b61a269c5267c976ab6f42f88deb2c4707
Walujo Emmanuel <walujoemmanuel@gmail.com>
2018-10-19 18:08:56 +0300

    admin can get al specific sale [Delivers #161279892] [Delivers #161279894]

--- app/app/views/sales.py
+++ app/app/views/sales.py
@@ -8,3 +8,12 @@ from app.models import sales
 def view_all_sales():
     return jsonify({"sales": sales}), 200
 
+@app.route('/api/v1/admin/sales/<int:sales_id>/', methods=['GET'])
+@app.route('/api/v1/attendant/sales/<int:sales_id>/', methods=['GET'])
+def view_asale_details(sales_id):
+    sale = [sale for sale in sales if sale["sales_id"] == sales_id]
+
+    if len(sale) == 0:
+        abort(404)
+
+    return jsonify({"sale": sale[0]}), 200
