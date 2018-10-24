1473743872d474b07d41197ec47b864a7fc4aab2
Walujo Emmanuel <walujoemmanuel@gmail.com>
2018-10-24 10:12:04 +0300

    modifying api endpoints and removing unused files

--- api/app/views/sales.py
+++ api/app/views/sales.py
@@ -24,6 +24,7 @@ def make_sale():
         return jsonify({"not_sold": "product not sold"})
 
 @app.route('/api/v1/admin/sales/', methods=['GET'])
+@app.route('/api/v1/attendant/sales/', methods=['GET'])
 def view_all_sales():
     return jsonify({"sales": sales}), 200
 
@@ -50,8 +51,8 @@ def edit_asale(sales_id):
             inventory = Sales(data["seller"], data["product"], data["price"], data["quantity"], data["total_cost"], data["date_sold"])
             if inventory.modify_sale(sale):
                 return jsonify({"sale_modified": f"sale of {sale[0]['product']} modified"}), 200
-            else:
-                abort(500)
+            # else:
+            #     abort(500)
     except:
         abort(500)
 
