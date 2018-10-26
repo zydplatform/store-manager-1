71e6014595ec78e91432a90c0b737428dbddd351
Walujo Emmanuel <walujoemmanuel@gmail.com>
2018-10-26 07:46:06 +0300

    Adding menu toggle in mobile devices UI

--- UI/assets/js/custom.js
+++ UI/assets/js/custom.js
@@ -1,24 +1,9 @@
 
-
-var loginForm = document.getElementById("login-form");
-var wrongUsername = document.getElementById('wrong-username');
-var wrongPasword = document.getElementById('wrong-pasword');
-
-loginForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-    var username = e.target.username.value;
-    var password = e.target.password.value;
-
-    if (username == '')
-        wrongUsername.style.visibility = 'visible';
-
-    if (password == '')
-        wrongPasword.style.visibility = 'visible';
-
-    if (username == 'admin' && password == 'admin')
-        window.location.href = 'admin/dashboard.html';
-
-    if (username == 'attendant' && password == 'attendant')
-        window.location.href = 'attendant/dashboard.html';
-    
-});
+// Menu togle
+function toggle_menu() {
+    var menu = document.getElementById('sidebar');
+    if ( menu.style.display == 'block' )
+        menu.style.display = 'none';
+    else
+        menu.style.display = 'block';
+}
